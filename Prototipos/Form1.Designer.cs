2cdfb33cea7a13bc69ff9b593916b651f100fea6
ENZO ANTEZANA <47AN43087541@campus.economicas.uba.ar>
2023-10-08 20:43:36 -0300

    Cambio 1

--- Prototipos/Form1.Designer.cs
+++ Prototipos/Form1.Designer.cs
@@ -28,12 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblNombre = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            textBox5 = new TextBox();
+            label4 = new Label();
+            textBox6 = new TextBox();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(12, 9);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(178, 15);
+            lblNombre.TabIndex = 0;
+            lblNombre.Text = "Nombre Completo/Razón Social";
+            lblNombre.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 27);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(226, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 88);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 2;
+            label1.Text = "CUIT/CUIL";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 153);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(105, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 135);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Correo Electrónico";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(137, 88);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 9;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(137, 70);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Teléfono";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(138, 153);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(138, 135);
+            label5.Name = "label5";
+            label5.Size = new Size(81, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Código Postal";
+            label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 201);
+            button1.Name = "button1";
+            button1.Size = new Size(59, 28);
+            button1.TabIndex = 12;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(176, 200);
+            button2.Name = "button2";
+            button2.Size = new Size(62, 29);
+            button2.TabIndex = 13;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(95, 201);
+            button3.Name = "button3";
+            button3.Size = new Size(59, 28);
+            button3.TabIndex = 14;
+            button3.Text = "Limpiar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(269, 254);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox6);
+            Controls.Add(label5);
+            Controls.Add(textBox5);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(lblNombre);
+            Name = "Form1";
+            Text = "Registro de Clientes";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblNombre;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private TextBox textBox4;
+        private Label label3;
+        private TextBox textBox5;
+        private Label label4;
+        private TextBox textBox6;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
